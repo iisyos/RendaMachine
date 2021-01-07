6ff7b2e62f9c177aedb4e14d84468c7a8d872bac
okachan <iisyos103@gmail.com>
2021-01-07 18:33:02 +0900

    renda-6

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,21 @@
 import { IonFab,IonIcon,IonFabButton,IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import { h } from 'ionicons/dist/types/stencil-public-runtime';
 import { type } from 'os';
-import React from 'react';
+import React  ,{ FunctionComponent } from 'react';
 import ReactDOM from 'react-dom';
 import './Home.css'
 import { IonButton,IonGrid, IonRow, IonCol,IonRippleEffect } from '@ionic/react';
 import { add, settings, share, person, arrowForwardCircle, arrowBackCircle, arrowUpCircle, logoVimeo, logoFacebook, logoInstagram, logoTwitter } from 'ionicons/icons';
-const Home: React.FC = () => {
+import { Link, RouteComponentProps, useHistory} from "react-router-dom";
+import Cookies from 'js-cookie'
+import { Plugins } from '@capacitor/core';
 
+const { Storage } = Plugins;
+
+type PageProps = {} & RouteComponentProps<{name: string}>;
+
+const Home: React.FC  <PageProps> = (props) => {
+  const history = useHistory();
   function Square(props:any){
     return <button className="square" onClick={props.onClick}>
       {/* {props.value} */}
@@ -15,72 +23,100 @@ const Home: React.FC = () => {
   }
 
 
-  class Board extends React.Component<{},{squares:any,xIsNext:boolean,counter:number}>{
+  class Board extends React.Component<{name:string},{squares:any,xIsNext:boolean,counter:number,pre_score:number}>{
     constructor(props:any){
       super(props);
       this.state={
         squares:Array(9).fill(null),
         xIsNext: true,
-        counter:0
+        counter:0,
+        pre_score:0
       }
     }
     handleClick(i:any) {
-      // const squares = this.state.squares.slice();
-      // squares[i] =this.state.xIsNext? 'X':"O";
-      // this.setState({squares: squares,xIsNext:!this.state.xIsNext});
       this.setState({counter:this.state.counter+1})
     }
     renderSquare(i:any){
-      return( <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />);
+      return(<div className="square2"><Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} /></div> );
+    }
+    async setObject() {
+      await Storage.set({
+        key: Cookies.get("namea")!,
+        value: JSON.stringify({
+          id: 1,
+          score: this.state.counter??0
+        })
+      });
+    }
+    async getObject() {
+      const ret = await Storage.get({ key: Cookies.get("namea")! });
+      console.log(JSON.parse(ret!.value!)+"-------")
+      const score = JSON.parse(ret!.value!)?.score??0;
+      this.setState({pre_score:score})
+    }
+
+    setScore(){
+      if(this.state.pre_score<this.state.counter){
+        this.setObject()
+      }
+      Cookies.set("name2",Cookies.get("namea")!)
+    }
+
+    componentDidMount(){
+      this.getObject()
     }
 
     render(){
       const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-
       return(
         <div>
           <IonGrid>
             <IonRow >
               <IonCol size="8" class="text-center">
-               playing
+            {Cookies.get("namea")}
               </IonCol>
               <IonCol>
-              <IonButton color="light" routerLink='/initial' class='round'>Quit</IonButton>
+              <IonButton color="light" routerLink='/initial' class='round' onClick={()=>this.setScore()}>Quit</IonButton>
               </IonCol>
             </IonRow>
-          </IonGrid>
-          <div><h1 className="ion-padding">{this.state.counter}</h1></div>
-        <div className="inner">
-        <div>
-          <div className="board-row">
+          <IonRow class="ion-justify-content-center"><div><h1 className="ion-padding">{this.state.counter}</h1></div></IonRow>
+          <div className="wrapper">
+            <IonRow class="ion-justify-content-center">
+          <IonRow>
             {this.renderSquare(0)}
             {this.renderSquare(1)}
             {this.renderSquare(2)}
-            </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-            </div>
-           <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div></div>
-        </div></div>
+            </IonRow>
+            <IonRow>
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+            </IonRow>
+            <IonRow>
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+            </IonRow>
+            </IonRow></div>
+        </IonGrid>
+        </div>
       )
     }
   }
 
-  class Game extends React.Component{
+  class Game extends React.Component<{name:string}>{
+    componentDidMount(){
+      console.log(props.match.params.name)
+    }
+
     render(){
       return(
-        <div><Board/></div>
+        <div><Board name="aaa"/></div>
       )
     }
   }
   ReactDOM.render(
-    <Game/>,document.getElementById('root')
+    <Game name={props.match.params.name}/>,document.getElementById('root')
   )
 
   return (
--- src/pages/Initial.tsx
+++ src/pages/Initial.tsx
@@ -4,38 +4,83 @@ import ReactDOM from 'react-dom';
 import './initial.css';
 import {  IonLabel,IonInput,IonItem,IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import { IonButton,IonGrid, IonRow, IonCol,IonRippleEffect } from '@ionic/react';
-import { dice } from 'ionicons/icons';
+import { analytics, dice } from 'ionicons/icons';
 import { h } from 'ionicons/dist/types/stencil-public-runtime';
-import { Router, Route, withRouter } from 'react-router';
-const Initial: React.FC =() =>{
+import { Router, Route, withRouter, RouteComponentProps } from 'react-router';
+import createBrowserHistory from 'history/createBrowserHistory';
+import { Link, useHistory} from "react-router-dom";
+import Cookies from 'js-cookie'
+import { Plugins } from '@capacitor/core';
+const { Storage } = Plugins;
+
+const Initial: React.FC<RouteComponentProps> =(props) =>{
+    let s:boolean=true;
+
 
     const [text, setText] = useState<string>();
+
     function Score_col(props:any){
         return (
             <IonCol className="text-center">
                 <div style={{color:'red'}}>
                     {props.value}
                 </div>
-                <div>~~~</div>
+                <div>{props.score}</div>
             </IonCol>
         )
 
     }
-    class Page extends React.Component{
+    
+    class Page extends React.Component<{},{score:number,initial:boolean}>{
+        
+        constructor(props:any){
+            super(props);
+            this.state={
+                score:0,
+                initial:true
+            }
+          }
+            async getObject() {
+                const ret = await Storage.get({ key: Cookies.get("namea")! });
+                console.log(JSON.parse(ret!.value!)+"-------")
+                const score = JSON.parse(ret!.value!)?.score??0;
+                this.setState({score:score})
+              }
+          componentDidMount(){
+              this.getObject()
+              if(Cookies.get("name2") && this.state.initial){
+                  console.log(text)
+                  if(text){Cookies.set("name2",text!)}
+                  setText(Cookies.get("name2")!)
+                  console.log(this.state.initial)
+                  this.setState({initial:false})
+                  console.log(this.state.initial)
+                  console.log(s+"aaaaaa")
+                  s=false;
+              }
+    // setText(Cookies.get("name2"))
+    Cookies.get("name2")??console.log(text+"aaaaaaaaaaaaaa")
+    // Cookies.get("name2")??console.log("qqqqqqqqqqqqqqqqq")
+    // Cookies.get("name2")?console.log("ppppppppppppqqqqqqqqqqqqqqqqq")
+          }
         render(){
             return(
                 <IonRow>
-                    <Score_col value="10s"/>
+                    <Score_col value="10s" score={this.state.score.toString()}/>
                     <Score_col value="60s"/>
                     <Score_col value="endless"/>
                 </IonRow>
             )
         }
     }
-
-    // ReactDOM.render(<Page/>,document.getElementById('root')
-
-    // )
+    const history = useHistory();
+    function NextPage(){
+        history.push({
+            pathname: '/home',
+            state: { name: text }
+          })
+          Cookies.set("namea",text!)
+    }
 
     return (
         <IonContent>
@@ -60,10 +105,12 @@ const Initial: React.FC =() =>{
                   <IonButton color="primary" className="round">endless</IonButton>
     </IonRow>
     <IonRow class="ion-justify-content-center">
-    <IonButton class="custom" routerLink="/home">PLAY</IonButton>
+    <IonButton class="custom" onClick={()=> NextPage()}>PLAY</IonButton>
     </IonRow>
           </IonGrid>
       </IonContent>
       );
 }
 export default Initial;
+
+// props.history.push({pathname:'/home',state:{name:"aaa"}})
