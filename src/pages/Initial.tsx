574d210c42c38221b6a32e992f23b6f00f3fbd5d
iisyos <67086449+iisyos@users.noreply.github.com>
2021-01-26 13:04:24 +0900

    new-1 (#14)
    Co-authored-by: takashi.asada <bhsqc767@gmail.com>

--- src/pages/Initial.tsx
+++ src/pages/Initial.tsx
@@ -21,8 +21,8 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
+
 import { makeStyles } from '@material-ui/core/styles';
 import { yellow } from '@material-ui/core/colors';
 
