79a7c1b64b555d272dbb05ef1f94690fe8e92666
takashi.asada <bhsqc767@gmail.com>
2021-01-26 12:59:53 +0900

    new-1

--- src/pages/Initial.tsx
+++ src/pages/Initial.tsx
@@ -21,9 +21,8 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
-import { makeStyles } from '@material-ui/core/styles';
+
 
 const { Storage } = Plugins;
 let s: boolean = true;
